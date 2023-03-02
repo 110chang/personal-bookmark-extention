38ec8f3e7d2530f568cbeb9a61476c242361bec7
Yuji Ito <u@110chang.com>
2023-03-02 17:30:17 +0900

    background.jsを追加

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    manifest: 'vite-manifest.json',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        background: path.resolve(__dirname, 'src/background.js'),
+      }
+    },
+  },
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
