07f6af1c72417606a79c0727d6209156c2fc818b
Yuji Ito <u@110chang.com>
2023-03-03 15:40:01 +0900

    background.jsからレスポンスが返るようにする

--- src/background.js
+++ src/background.js
@@ -1,36 +1,35 @@
-console.log('background')
+import { logIn, logOut, refreshAuth, postBookmarks } from './lib/api'
 
-chrome.runtime.onMessage.addListener(async (request) => {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   switch (request.name) {
     case 'login:background':
-      await fetch('http://127.0.0.1:5000/api/login', {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        credentials: 'include',
-        body: JSON.stringify({ username: 'user', password: 'password' }),
+      logIn({ username: request.data.username, password: request.data.password }).then((res) => {
+        console.log(res)
+        sendResponse(res)
       })
-      return
+      return true
 
     case 'logout:background':
-      await fetch('http://127.0.0.1:5000/api/logout', {
-        method: 'delete',
-        credentials: 'include',
+      logOut().then((res) => {
+        console.log(res)
+        sendResponse(res)
       })
-      return
+      return true
+
+    case 'refresh:background':
+      refreshAuth({ id: request.data.id }).then((res) => {
+        console.log(res)
+        sendResponse(res)
+      })
+      return true
 
     case 'postBookmark:background':
       console.log(request.data)
-      await fetch('http://127.0.0.1:5000/api/bookmarks', {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        credentials: 'include',
-        body: JSON.stringify({ title: request.data.title, url: request.data.url }),
+      postBookmarks({ title: request.data.title, url: request.data.url }).then((res) => {
+        console.log(res)
+        sendResponse(res)
       })
-      return
+      return true
 
     default: return
   }
