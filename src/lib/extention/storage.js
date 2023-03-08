46c7b99d5ae94e19d588daecb82576d58a7fc66c
Yuji Ito <u@110chang.com>
2023-03-08 15:13:39 +0900

    認証をrefreshできるようにした

--- src/lib/extention/storage.js
+++ src/lib/extention/storage.js
@@ -1,14 +1,14 @@
 export async function saveUser({ id = '', username = '' } = {}) {
-  await chrome.storage.session.set({ user: JSON.stringify({ id, username }) })
+  await chrome.storage.local.set({ user: JSON.stringify({ id, username }) })
 }
 
 export async function getUser() {
-  const res = await chrome.storage.session.get(['user'])
+  const res = await chrome.storage.local.get(['user'])
   return res.user ? JSON.parse(res.user) : null
 }
 
 export async function clearUser() {
-  await chrome.storage.session.remove(['user'])
+  await chrome.storage.local.remove(['user'])
 }
 
 export default {
--- src/lib/storage.js
+++ src/lib/storage.js
@@ -2,27 +2,27 @@ import extStorage from './extention/storage'
 import webStorage from './web/storage'
 
 export async function saveUser({ id = '', username = '' } = {}) {
-  if (window.sessionStorage) {
-    webStorage.saveUser({ id, username })
-  } else if (chrome.storage) {
+  if (chrome.storage) {
     await extStorage.saveUser({ id, username })
+  } else if (window.sessionStorage) {
+    webStorage.saveUser({ id, username })
   }
 }
 
 export async function getUser() {
-  if (window.sessionStorage) {
-    return await Promise.resolve(webStorage.getUser())
-  } else if (chrome.storage) {
+  if (chrome.storage) {
     return await extStorage.getUser()
+  } else if (window.sessionStorage) {
+    return await Promise.resolve(webStorage.getUser())
   }
   return null
 }
 
 export async function clearUser() {
-  if (window.sessionStorage) {
-    webStorage.clearUser()
-  } else if (chrome.storage) {
+  if (chrome.storage) {
     await extStorage.clearUser()
+  } else if (window.sessionStorage) {
+    webStorage.clearUser()
   }
 }
 
